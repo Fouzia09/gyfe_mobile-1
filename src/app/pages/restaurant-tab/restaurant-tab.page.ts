40fd6b20757a933c38c6b302c1459587692c4219
FOUZIA HAJ <hfouzia@hotmail.fr>
2022-03-14 23:01:47 +0100

    ajout recup restaurants

--- src/app/pages/restaurant-tab/restaurant-tab.page.ts
+++ src/app/pages/restaurant-tab/restaurant-tab.page.ts
@@ -1,4 +1,7 @@
+import { Restaurant, RestaurantOUT } from './../../interfaces/restaurant';
 import { Component } from '@angular/core';
+import { RestaurantService } from 'src/app/services/restaurant.service';
+import { UserOUT } from 'src/app/interfaces/user';
 
 @Component({
   selector: 'app-restaurant-tab',
@@ -7,6 +10,40 @@ import { Component } from '@angular/core';
 })
 export class RestaurantTabPage {
 
-  constructor() {}
+  listrestaurants!: Restaurant[];
+  isLoading = false;
+  errorApi = false;
+  success = false;
+  restaurants!: RestaurantOUT[];
+
+
+  constructor(
+    private restaurantService: RestaurantService,
+    //private matDialog: MatDialog,
+    ) {
+    this.getRestaurants();
+   }
+
+  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+  ngOnInit(): void {
+  }
+
+  getRestaurants(): void {
+    let userLoggedInfo: UserOUT | string;
+    userLoggedInfo = localStorage.getItem('userLoggedInfo') as string;
+    userLoggedInfo = JSON.parse(userLoggedInfo) as UserOUT;
+    this.restaurants = userLoggedInfo.restaurants as RestaurantOUT[];
+    console.log(this.restaurants);
+  }
+
+  //  getRestaurants(){
+  //     this.restaurantService.getRestaurants().subscribe(
+  //       (      data: Restaurant[])=>{
+  //         this.listrestaurants = data;
+  //       }
+  //     );
+  //   };
+
+
 
 }

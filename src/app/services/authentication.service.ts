8fa9112d72a34fc79a7c83eb8668c40d421c517b
YasminaAston <yasmina.kondgbia@social.aston-ecole.com>
2022-03-14 18:09:21 +0100

    Room page

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -2,16 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { API_ROUTE } from '../routes/api-routes';
-import { User, UserOUT, UserToken } from '../interfaces/user';
-import { Router } from '@angular/router';
+import { User, UserOUT } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+
   private tokenName = 'token';
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient) { }
 
   public logOn(identifiant: any): any{
     return this.http.post(API_ROUTE.logon, identifiant);
@@ -63,63 +63,5 @@ export class AuthenticationService {
 
   resetPassword(body: any, email: string): Observable<any> {
     return this.http.post<any>(`${API_ROUTE.users}/resetPassword/${email}`, body);
-  }
-
-  forgotPassword(email: string): Observable<any> {
-    return this.http.post<any>(`${API_ROUTE.users}/forgot-password`, email);
-  }
-
-
-  setInLocalStorage(key: string, value: any): void {
-    localStorage.setItem(key, value);
-  }
-
-  removeInLocalStorage(key: string): void {
-    localStorage.removeItem(key);
-  }
-
-  logout(){
-    localStorage.clear();
-    //déconnection
-    this.router.navigate(['/authentication/login']);
-  }
-
-  isLogged(){
-    return this.getToken() !== null;
-  }
-
-  public userLoggedUsername(): string  {
-    const token = this.tokenDecoded() as UserToken;
-    const username = token.username;
-    return username;
-  }
-
-  public userLoggedRoles(): string[] {
-    const token = this.tokenDecoded() as UserToken;
-    const roles = token.roles;
-    return roles;
-  }
-
-  public tokenExpiration(): number {
-    const token = this.tokenDecoded() as UserToken;
-    const exp = token.exp;
-    return exp;
-  }
-
-  public userLoggedEmail(): string {
-    const token = this.tokenDecoded() as UserToken;
-    const email = token.email;
-    return email;
-  }
-
-  private tokenDecoded(): UserToken | boolean {
-    const token = this.getToken();
-    if (token) {
-      const decodedToken = atob(token.split('.')[1]);
-      const decodedTokenJsonFormat = JSON.parse(decodedToken);
-      return decodedTokenJsonFormat;
-    } else {
-      return false;
-    }
   }
 }

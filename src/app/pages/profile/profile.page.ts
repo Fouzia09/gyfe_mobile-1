cc7e6b34aa8a6905e0f45272197957a4d4f1c3d1
YasminaAston <yasmina.kondgbia@social.aston-ecole.com>
2022-03-14 17:32:03 +0100

    Typescript list room

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,4 +1,8 @@
+import { AuthenticationService } from './../../services/authentication.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'ionic';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +11,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+    //@ts-ignore
+    currentuser: User;
 
-  ngOnInit() {
+    username!: string;
+    roles!: string[];
+    email!: string;
+    constructor(
+      private auth: AuthenticationService,
+      private router: Router,
+      private currentuserService: UserService,
+    ) { }
+
+    ngOnInit(): void {
+       this.username =this.auth.userLoggedUsername();
+       this.roles =this.auth.userLoggedRoles();
+      this.email = this.auth.userLoggedEmail();
+
+      //la déconnection
+      if(!this.auth.isLogged()){
+        this.router.navigate(['/authentication/login']);
+      }
+
+      this.getCurrentUser();
+    }
+
+    getCurrentUser(){
+      this.currentuserService.getCurrentUser().subscribe(
+        (data)=>{
+          //@ts-ignore
+          this.currentuser = data;
+        }
+      );
+    }
+
+    //déconnexion
+    logout(){
+      this.auth.logout();
+    }
+
+    /* onOpenDialogEditPassword(currentuser: User) {
+      let dialogRef = this.matDialog.open(PasswordEditComponent,
+        {
+          data: {
+            id: currentuser.id,
+            plainPassword: currentuser.plainPassword,
+          },
+          width: "500px",
+          height: "450px",
+        })
+    } */
+
   }
-
-}
--- src/app/pages/room-tab/room-tab.page.ts
+++ src/app/pages/room-tab/room-tab.page.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Room, RoomOUT } from 'src/app/interfaces/room';
+import { UserOUT } from 'src/app/interfaces/user';
+import { RoomService } from 'src/app/services/room.service';
 
 @Component({
   selector: 'app-room-tab',
   templateUrl: 'room-tab.page.html',
   styleUrls: ['room-tab.page.scss']
 })
-export class RoomTabPage {
+  export class RoomAdminListComponent implements OnInit {
 
-  constructor() {}
+    listroom!: Room[];
+    isLoading = false;
+    errorApi = false;
+    success = false;
+    rooms!: RoomOUT[];
 
-}
+
+    constructor(
+      private roomService: RoomService,
+      //private matDialog: MatDialog,
+      ) {
+      this.getRooms();
+     }
+
+    ngOnInit(): void {
+    }
+
+    getRooms(): void {
+      let userLoggedInfo: UserOUT | string;
+      userLoggedInfo = localStorage.getItem('userLoggedInfo') as string;
+      userLoggedInfo = JSON.parse(userLoggedInfo) as UserOUT;
+      this.rooms = userLoggedInfo.rooms as RoomOUT[];
+    }
+
+    /* getRooms(){
+      this.roomService.getListRoom().subscribe(
+        (      data: Room[])=>{
+          this.listroom = data;
+        }
+      )
+    }
+   */
+
+
+  }
+
+
+
